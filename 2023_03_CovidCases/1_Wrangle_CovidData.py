bd24325de41dd7fdf193122323aeed148ef75fed
Ben Richmond <ben_richmond@ntlworld.com>
2023-03-05 11:16:20 +0000

    added in further testing for python e.g. seaborn

--- 2023_03_CovidCases/1_Wrangle_CovidData.py
+++ 2023_03_CovidCases/1_Wrangle_CovidData.py
@@ -1,19 +1,23 @@
-#%% Import packages
+# 1_Wrangle_CovidData
+# Th
+# %% Import packages
 import pandas as pd
 from siuba import *
 from siuba.dply.vector import *
 from skimpy import clean_columns
+import seaborn as sb
 
 # %% Import Covid data
-covid_data = clean_columns(pd.read_csv('2023_03_CovidCases/Country-wise-COVID-cases.csv'))
-
-print(covid_data)
+covid_data = clean_columns(pd.read_csv('data/Country-wise-COVID-cases.csv'))
 
 # %% Siuba testing
 su_test = (
     covid_data >>
-    select(_.country_name)
+    summarize(sum = _.total_infected.sum()/1000000)
 )
 print(su_test)
+
+# %% plot using seaborn
+sb.relplot(data=covid_data, x="total_infected", y="total_deaths")
 
 # %%
